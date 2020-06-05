6801a18363a04d0769b5dc9feba431f92051fda8
MERTKIRKALI <mertmehnurkirkali@gmail.com>
2020-06-05 13:38:02 +0300

    new test cases added.

--- tests/getir.test.js
+++ tests/getir.test.js
@@ -18,18 +18,35 @@ const badRequest = {
     }
 }
 
-test('Checks request', () => {
+test('Checks request with good request', () => {
     let res = { status: jest.fn() }, next = jest.fn();
     app.checkRequestMiddleware(goodRequest, res, next);
     expect(next).toHaveBeenCalled();
 
 })
 
+test('Checks request with bad request.', () => {
+    let res = {
+        status: jest.fn((status => {
+            let send = { send: jest.fn() };
+            return send;
+        }))
+    }, next = jest.fn();
+    app.checkRequestMiddleware(badRequest, res, next);
+    expect(next).not.toHaveBeenCalled();
+
+})
 
 
-it('Fetch records', () => {
+
+it('Fetch records with good request', () => {
     expect.assertions(1);
     return business.fetchRecords(goodRequest.body).then(data => expect(data.length).toBeGreaterThan(0));
+});
+
+it('Fetch records with bad request', () => {
+    expect.assertions(1);
+    return business.fetchRecords(badRequest.body).then(data => expect(data.length).toEqual(0));
 });
 
 
