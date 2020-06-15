4704ab78c3fbc69343a68a1bda21a8b08e78d65f
MERTKIRKALI <mertmehnurkirkali@gmail.com>
2020-06-15 18:18:31 +0300

    db uri update

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 let startConnection = function () {
-    return mongoose.connection.openUri('mongodb://dbUser:dbPassword1@ds249623.mlab.com:49623/getir-case-study', {
+    return mongoose.connection.openUri(' mongodb+srv://challengeUser:WUMglwNBaydH8Yvu@challenge-xzwqd.mongodb.net/' + 'getir-case-study?retryWrites=true', {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
